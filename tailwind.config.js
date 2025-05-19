6a8a6b44df6ae60d436349e1ce04bdc966306208
itmilos <mikeruje@gmail.com>
2025-05-19 09:09:26 -0400

    first v

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#222831',
+        secondary: '#393E46',
+        accent: '#00ADB5',
+        light: '#EEEEEE',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
